f58193d631fbb148cf4e456fda3fc80cc7e9e71b
Vinay Arora <vinay.arora7171@hotmail.com>
2020-04-10 12:49:28 +0530

    added date component

--- dynamic-form/src/app/components/date/date.component.ts
+++ dynamic-form/src/app/components/date/date.component.ts
@@ -1,20 +1,24 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit } from "@angular/core";
+import { FormGroup } from "@angular/forms";
+import { FieldConfig } from "../../field.interface";
 @Component({
-  selector: 'app-date',
+  selector: "app-date",
   template: `
-    <p>
-      date works!
-    </p>
-  `,
-  styles: [
-  ]
+<mat-form-field class="demo-full-width margin-top" [formGroup]="group">
+<input matInput [matDatepicker]="picker" [formControlName]="field.name" [placeholder]="field.label">
+<mat-datepicker-toggle matSuffix [for]="picker"></mat-datepicker-toggle>
+<mat-datepicker #picker></mat-datepicker>
+<mat-hint></mat-hint>
+<ng-container *ngFor="let validation of field.validations;" ngProjectAs="mat-error">
+<mat-error *ngIf="group.get(field.name).hasError(validation.name)">{{validation.message}}</mat-error>
+</ng-container>
+</mat-form-field>
+`,
+  styles: []
 })
 export class DateComponent implements OnInit {
-
+  field: FieldConfig;
+  group: FormGroup;
   constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit() { }
 }
